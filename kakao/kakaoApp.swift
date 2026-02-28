ac21b47d439a32838f481819aede6c9ad0e04f25
nayu104 <ose794@gmail.com>
2026-03-01 05:54:07 +0900

    Initial Commit

---
+++ kakao/kakaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakaoApp.swift
+//  kakao
+//
+//  Created by nayu on 2026/03/01.
+//
+
+import SwiftUI
+
+@main
+struct kakaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kakaoTests/kakaoTests.swift
@@ -0,0 +1,17 @@
+//
+//  kakaoTests.swift
+//  kakaoTests
+//
+//  Created by nayu on 2026/03/01.
+//
+
+import Testing
+@testable import kakao
+
+struct kakaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
